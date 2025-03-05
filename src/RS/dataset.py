0e13005737a723bf9e17e71c94234dab72d3fa9c
Vanius <1297000341@qq.com>
2025-03-05 11:34:40 +0000

    first commit

---
+++ src/RS/dataset.py
@@ -0,0 +1,155 @@
+import pickle
+import os
+import numpy as np
+
+import torch
+import torch.utils.data as Data
+
+from utils import load_json, load_pickle
+
+
+class MyDataset(Data.Dataset):
+    def __init__(self, data_path, set='train', task='ctr', max_hist_len=10,
+                 augment=False, aug_prefix=None, kg_aug=False, kg_aug_dim=100,
+                 group_aug=False, load=False):
+        self.task = task
+        self.max_hist_len = max_hist_len
+        self.augment = augment
+        self.kg_aug = kg_aug
+        self.kg_aug_dim = kg_aug_dim
+        self.group_aug = group_aug
+        self.set = set
+        self.data = load_pickle(data_path + f'/{task}.{set}')
+        if self.group_aug:
+            self.stat = load_json(data_path + f'/{aug_prefix}_group_stat.json')
+        elif self.augment:
+            self.stat = load_json(data_path + f'/{aug_prefix}_stat.json')
+        else:
+            self.stat = load_json(data_path + f'/stat.json')
+        self.item_num = self.stat['item_num']
+        self.attr_num = self.stat['attribute_num']
+        self.attr_ft_num = self.stat['attribute_ft_num']
+        self.rating_num = self.stat['rating_num']
+        self.dense_dim = self.stat['dense_dim']
+        # print('dense dim!!!', self.dense_dim)
+        if task == 'rerank':
+            self.max_list_len = self.stat['rerank_list_len']
+        self.length = len(self.data)
+        self.sequential_data = load_json(data_path + '/sequential_data.json')
+        self.item2attribution = load_json(data_path + '/item2attributes.json')
+        datamaps = load_json(data_path + '/datamaps.json')
+        self.id2item = datamaps['id2item']
+        self.id2user = datamaps['id2user']
+        self.aug_num = 0
+        if group_aug:
+            self.hist_aug_data = load_json(data_path + f'group_data/{aug_prefix}_augment.hist')
+            self.item_aug_data = load_json(data_path + f'group_data/{aug_prefix}_augment.item')
+            self.group_item_aug_data = load_json(data_path + f'group_data/{aug_prefix}_augment.gitm')
+            self.group_pos_hst_aug_data = load_json(data_path + f'group_data/{aug_prefix}_augment.gphst')
+            self.group_neg_hst_aug_data = load_json(data_path + f'group_data/{aug_prefix}_augment.gnhst')
+            self.item_group_map = load_json(data_path + f'group_data/item/result/id2group.json')
+            self.user_group_map = load_json(data_path + f'group_data/user/id2pos_neg_group.json')
+            self.aug_num += 5
+        else:
+            if augment:
+                if load:
+                    print('load original vector')
+                    self.hist_aug_data = load_json(data_path + f'/{aug_prefix}_augment.hist')
+                    self.item_aug_data = load_json(data_path + f'/{aug_prefix}_augment.item')
+                else:
+                    self.hist_aug_data = load_json(data_path + f'/{aug_prefix}_augment.hist', float_type=np.float32)
+                    self.item_aug_data = load_json(data_path + f'/{aug_prefix}_augment.item', float_type=np.float32)
+                self.aug_num += 2
+            # print('item key', list(self.item_aug_data.keys())[:6], len(self.item_aug_data), self.item_num)
+
+        if kg_aug:
+            raw_kg_map = load_json(os.path.join(data_path, 'KGE', 'itm_ent_embed.map'))
+            default = [0 for _ in range(kg_aug_dim)]
+            self.kg_map = {iid: raw_kg_map.get(item, default) for iid, item in self.id2item.items()}
+            self.aug_num += 1
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, _id):
+        if self.task == 'ctr':
+            uid, seq_idx, lb = self.data[_id]
+            item_seq, rating_seq = self.sequential_data[str(uid)]
+            iid = item_seq[seq_idx]
+            hist_seq_len = seq_idx - max(0, seq_idx - self.max_hist_len)
+            attri_id = self.item2attribution[str(iid)]
+            hist_item_seq = item_seq[max(0, seq_idx - self.max_hist_len): seq_idx]
+            hist_rating_seq = rating_seq[max(0, seq_idx - self.max_hist_len): seq_idx]
+            hist_attri_seq = [self.item2attribution[str(idx)] for idx in hist_item_seq]
+            out_dict = {
+                'iid': torch.tensor(iid).long(),
+                'aid': torch.tensor(attri_id).long(),
+                'lb': torch.tensor(lb).long(),
+                'hist_iid_seq': torch.tensor(hist_item_seq).long(),
+                'hist_aid_seq': torch.tensor(hist_attri_seq).long(),
+                'hist_rate_seq': torch.tensor(hist_rating_seq).long(),
+                'hist_seq_len': torch.tensor(hist_seq_len).long()
+            }
+            if self.augment:
+                item_aug_vec = self.item_aug_data[str(self.id2item[str(iid)])]
+                # print(self.id2user[str(uid)])
+                # print(list(self.hist_aug_data.keys())[:50])
+                hist_aug_vec = self.hist_aug_data[str(self.id2user[str(uid)])]
+                out_dict['item_aug_vec'] = torch.tensor(item_aug_vec).float()
+                out_dict['hist_aug_vec'] = torch.tensor(hist_aug_vec).float()
+
+            if self.kg_aug:
+                kg_aug_vec = self.kg_map[str(iid)]
+                out_dict['kg_aug_vec'] = torch.tensor(kg_aug_vec).float()
+            if self.group_aug:
+                item_group_aug_vec = self.group_item_aug_data[str(self.item_group_map[str(iid)])]
+                user_pos_group_aug_vec = self.group_pos_hst_aug_data[str(self.user_group_map[str(uid)][1])]
+                user_neg_group_aug_vec = self.group_neg_hst_aug_data[str(self.user_group_map[str(uid)][0])]
+                # try:
+                #     user_neg_group_aug_vec = self.group_neg_hst_aug_data[str(self.user_group_map[str(uid)][0])]
+                # except:
+                #     print(str(uid))
+                #     print(str(self.user_group_map[str(uid)][0]))
+                #     print(self.group_neg_hst_aug_data.keys())
+                #     print(self.group_neg_hst_aug_data[str(self.user_group_map[str(uid)][0])])
+                out_dict['item_group_aug_vec'] = torch.tensor(item_group_aug_vec).float()
+                out_dict['user_pos_group_aug_vec'] = torch.tensor(user_pos_group_aug_vec).float()
+                out_dict['user_neg_group_aug_vec'] = torch.tensor(user_neg_group_aug_vec).float()
+        elif self.task == 'rerank':
+            uid, seq_idx, candidates, candidate_lbs = self.data[_id]
+            candidates_attr = [self.item2attribution[str(idx)] for idx in candidates]
+            item_seq, rating_seq = self.sequential_data[str(uid)]
+            hist_seq_len = seq_idx - max(0, seq_idx - self.max_hist_len)
+            hist_item_seq = item_seq[max(0, seq_idx - self.max_hist_len): seq_idx]
+            hist_rating_seq = rating_seq[max(0, seq_idx - self.max_hist_len): seq_idx]
+            hist_attri_seq = [self.item2attribution[str(idx)] for idx in hist_item_seq]
+            out_dict = {
+                'iid_list': torch.tensor(candidates).long(),
+                'aid_list': torch.tensor(candidates_attr).long(),
+                'lb_list': torch.tensor(candidate_lbs).long(),
+                'hist_iid_seq': torch.tensor(hist_item_seq).long(),
+                'hist_aid_seq': torch.tensor(hist_attri_seq).long(),
+                'hist_rate_seq': torch.tensor(hist_rating_seq).long(),
+                'hist_seq_len': torch.tensor(hist_seq_len).long()
+            }
+            if self.augment:
+                item_aug_vec = [torch.tensor(self.item_aug_data[str(self.id2item[str(idx)])]).float()
+                                for idx in candidates]
+                hist_aug_vec = self.hist_aug_data[str(self.id2user[str(uid)])]
+                out_dict['item_aug_vec_list'] = item_aug_vec
+                out_dict['hist_aug_vec'] = torch.tensor(hist_aug_vec).float()
+            if self.group_aug:
+                item_group_aug_vec = [torch.tensor(self.group_item_aug_data[str(self.item_group_map[str(iid)])]).float()
+                                      for iid in candidates]
+                user_pos_group_aug_vec = self.group_pos_hst_aug_data[str(self.user_group_map[str(uid)][1])]
+                user_neg_group_aug_vec = self.group_neg_hst_aug_data[str(self.user_group_map[str(uid)][0])]
+
+                out_dict['item_group_aug_vec_list'] = item_group_aug_vec
+                out_dict['user_pos_group_aug_vec'] = torch.tensor(user_pos_group_aug_vec).float()
+                out_dict['user_neg_group_aug_vec'] = torch.tensor(user_neg_group_aug_vec).float()
+        else:
+            raise NotImplementedError
+
+        return out_dict
+
+
---
+++ src/RS/run_ctr.py
@@ -0,0 +1,95 @@
+import subprocess
+import os
+import sys
+
+# dataset_name = 'ml-1m'
+dataset_name = 'Yelp_raw_data'
+# dataset_name = 'ml-25m'
+
+# Training args
+data_dir = f'/home/tongwan/{dataset_name}/proc_data_0/'
+task_name = 'ctr'
+layer = -1  # which layer of LLM
+# aug_prefix = f'embeddings/marc_avg_all_layer{layer}'  # for original LLM representation
+aug_prefix = f'embeddings/marc_avg_comp_layer{layer}'  # for compressed LLM representation
+
+print('aug prefix: ', aug_prefix)
+# augment = True
+augment = False
+
+
+epoch = 20
+batch_size = 256
+lr = '5e-4'
+lr_sched = 'cosine' #
+weight_decay = 0
+
+model = 'DIN'
+# model = 'DCNv1'
+# model = 'DCNv2'
+# model = 'DeepFM'
+# model = 'xDeepFM'
+# model = 'FiBiNet'
+# model = 'FiGNN'
+# model = 'AutoInt'
+embed_size = 32
+final_mlp = '200,80'
+num_cross_layers = 3
+dropout = 0.0
+max_hist_len = 10
+
+convert_type = 'MoE'
+convert_arch = '128,32'
+convert_dropout = 0.0
+expert_num = 2
+
+
+if dataset_name == 'ml-25m':  # ml-25m is much slower than other dataset
+    bss = [256, 512]
+    # bss = [128, 1024]
+    experts = [2, 3, 4]
+    num_worker = 8
+else:
+    bss = [256, 128, 512, 1024]
+    # experts = [1, 2, 3]
+    # experts = [4, 5, 6]
+    experts = [1, 2, 3, 4, 5, 6]
+    num_worker = 4
+
+if dataset_name == 'yelp':
+    lrs = ['1e-4', '2e-4', '5e-4', '1e-3']
+else:
+    lrs = ['1e-4', '5e-4', '1e-3', '2e-3']
+
+# Run the train process
+for lr_sched in ['cosine']:
+    for batch_size in bss:
+        for lr in lrs:
+            for expert_num in experts:
+                print('---------------bs, lr, epoch, expert , convert arch, gru----------', batch_size,
+                      lr, epoch, expert_num, convert_arch, model)
+                subprocess.run(['python', '-u', 'main_ctr.py',
+                                f'--save_dir=./model/{dataset_name}/{task_name}/{model}/WDA_Emb{embed_size}_epoch{epoch}'
+                                f'_bs{batch_size}_lr{lr}_{lr_sched}_cnvt_arch_{convert_arch}_cnvt_type_{convert_type}'
+                                f'_eprt_{expert_num}_wd{weight_decay}_drop{dropout}' + \
+                                f'_hl{final_mlp}_cl{num_cross_layers}_augment_{augment}',
+                                f'--data_dir={data_dir}',
+                                f'--augment={augment}',
+                                f'--aug_prefix={aug_prefix}',
+                                f'--task={task_name}',
+                                f'--max_hist_len={max_hist_len}',
+                                f'--convert_arch={convert_arch}',
+                                f'--convert_type={convert_type}',
+                                f'--convert_dropout={convert_dropout}',
+                                f'--epoch_num={epoch}',
+                                f'--num_worker={num_worker}',
+                                f'--batch_size={batch_size}',
+                                f'--lr={lr}',
+                                f'--lr_sched={lr_sched}',
+                                f'--weight_decay={weight_decay}',
+                                f'--algo={model}',
+                                f'--embed_size={embed_size}',
+                                f'--expert_num={expert_num}',
+                                f'--final_mlp_arch={final_mlp}',
+                                f'--dropout={dropout}',
+                                ])
